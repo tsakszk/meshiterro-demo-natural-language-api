21cb0383e6e0ab511108b1b1b285fd39d20a9cff
toshiaki suzuki <toshiaki1986@gmail.com>
2020-06-16 07:14:31 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  mount_uploader :profile_image, ImageUploader
+  mount_uploader :profile_image_id, ImageUploader
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
