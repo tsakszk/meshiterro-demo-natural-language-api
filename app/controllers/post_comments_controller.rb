025f5a5afe8948910ec964ead5cfb0e92f395fb2
toshiaki suzuki <toshiaki1986@gmail.com>
2020-06-16 08:22:14 +0000

    2nd_commit

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,12 +3,11 @@ class PostCommentsController < ApplicationController
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
+    comment.score = Language.get_data(post_comment_params[:comment])
     comment.save
     redirect_to post_image_path(post_image)
   end
-
   private
-
   def post_comment_params
     params.require(:post_comment).permit(
       :user_id,
